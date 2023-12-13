bcc599425ce34404ee10c3feaa850287af62aae8
Алиева Замира Бейсенбековна <AlievaZ@lcoast.itstep>
2023-12-13 21:44:06 +0600

    Fixes

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ height=float(input("Вваедите свой рост в сантиметрах
 bmi=bmi_calulator(weight,height)
 
 if bmi<18.5:
-    print(f'У вас недовес.Ваш показатель bmi={bmi}')
-elif bmi>18.5 and bni<24.9:
-    print(f'у вас перевес.Ваш показатель bmi={bmi}')
+    print(f'У вас недовес. Ваш показатель bmi={bmi}')
+elif bmi>18.5 and bmi<24.9:
+    print(f'Все Отлично. Ваш показатель bmi={bmi}')
 else:
-    print(f'У вас перевес.Ваш паказатель bmi={bmi}')
+    print(f'У вас перевес. Ваш паказатель bmi={bmi}')
